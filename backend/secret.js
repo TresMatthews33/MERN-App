c02e6851c74ec717769e2f3fa4196fb6d858ae3a
Tres Matthews <tres.matthews7@gmail.com>
2019-11-25 10:11:40 -0600

    Added in gitignore to ignore modules, installed packages for the server

--- backend/secret.js
+++
@@ -1,8 +0,0 @@
-const secrets= {
-    //The URL that we use to connect to the MongoDB Atlas cluster
-    dbUri: 'mongodb+srv://TresMatthews:<1939574685>@cluster0-yg9gp.mongodb.net/test?retryWrites=true&w=majority'
-};
-
-const getSecret = key => secrets[key];
-
-module.exports = getSecret;
